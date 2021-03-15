a33688b9265516cdaa17d55e6fdbc0634ed86a44
INFOCORP\Casey.Dietz <caseydietz20@gmail.com>
2021-03-14 22:50:46 -0600

    MVP

--- react-lifecycle/src/App.js
+++ react-lifecycle/src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import UserCard from "./components/UserCard"
+import UserCard from "./components/UserCard";
+import axios from 'axios';
+
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      userData: []
+  state = {
+    user: [],
+    followerData: []
+  }
+
+  componentDidMount() {
+      axios
+        .all([
+        axios.get('https://api.github.com/users/casey-dietz'),     
+        axios.get('https://api.github.com/users/casey-dietz/followers'),
+       ])    
+        .then(
+          axios.spread((res1, res2) => {
+            console.log(res1, res2)
+            this.setState({
+              user: res1.data,
+              followerData: res2.data
+            })
+          })
+        )
+        .catch(err => console.log(err))
     }
 
-  }
   render() {
     return (
-      <UserCard />
+      <UserCard user={this.state.user} followerData={this.state.followerData} />
     )
   }
 }
--- react-lifecycle/src/components/UserCard.js
+++ react-lifecycle/src/components/UserCard.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+    img {
+        display: flex;
+        justify-content: center;
+        width: 200px;
+        margin: auto;
+    }
+
+`
 
 const UserCard = (props) => {
+    const {user, followerData} = props;
+
     return (
-        <div className='card'>
-            
-        </div>
+        <StyledDiv className='card'>
+            <h1>Github User Card</h1>
+            <img src={user.avatar_url} alt={user.login}/>
+            <h2>Name: {user.name}</h2>
+            <p>Location: {user.location}</p>
+            <p>Followers: {user.followers}</p>
+            <p>Following: {user.following}</p>
+            <div className='followers'>
+                {followerData.map(follower => {
+                    return (
+                        <div key={follower.id}>
+                            <img src={follower.avatar_url} alt={follower.login}/>
+                            <p>Username: {follower.login}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </StyledDiv>
     )
 }
 
